d7c964888ebfcc0fd55e791d2e820dfb3fc22145
yaw <peiyawei@lczq.com>
2021-08-21 16:29:06 +0800

    基础学习：函数、类

--- test/a.py
+++ test/a.py
@@ -186,7 +186,7 @@ files.extend(["f3.txt", "f4.txt"])
 print("extend", files)
 
 # 按位置添加
-files.insert(1, "file5.txt")     # 添加入第1位（首位是0）
+files.insert(1, "file5.txt")  # 添加入第1位（首位是0）
 print("insert", files)
 
 # 移除某索引
@@ -197,7 +197,7 @@ print("del", files)
 files.remove("f3.txt")
 print("remove", files)
 
-files = {"ID": 111, "passport": "my passport", "books": [1,2,3]}
+files = {"ID": 111, "passport": "my passport", "books": [1, 2, 3]}
 
 # 按key拿取，并在拿取失败的时候给一个设定好的默认值
 print('files["ID"]:', files["ID"])
@@ -212,3 +212,163 @@ popped = files.pop("ID")
 print('popped:', popped)
 print("remain:", files)
 print("<=======", "数据种类", "=======")
+
+print("=======", "函数", "=======>")
+
+
+def f(x, a, b, c):
+    return a * x ** 2 + b * x + c * 1
+
+
+print(f(2, 1, 1, 0))
+print(f(x=2, a=1, b=1, c=0))
+print(f(c=0, b=1, a=1, x=2))
+
+
+def f(x, a=1, b=1, c=0):
+    return a * x ** 2 + b * x + c * 1
+
+
+print(f(2, a=2))
+print(f(2))
+
+filename = "f1.txt"
+
+
+def modify_name():
+    filename = "f2.txt"
+    print("local filename:", filename)
+
+
+modify_name()
+print("global filename:", filename)
+
+filename = "f1.txt"
+
+
+def modify_name():
+    global filename  # 提出申请
+    filename = "f2.txt"
+    print("local filename:", filename)
+
+
+modify_name()
+print("global filename:", filename)
+print("<=======", "函数", "=======")
+
+print("=======", "Class 类", "=======>")
+
+
+class File:
+    def __init__(self):
+        self.name = "f1"
+        self.create_time = "today"
+
+
+my_file = File()
+print(my_file.name)
+print(my_file.create_time)
+my_file.name = "new_name"
+print(my_file.name)
+
+
+class File:
+    def __init__(self, name, create_time="today"):
+        self.name = name
+        self.create_time = create_time
+
+
+my_file = File("my_file")
+print(my_file.name)
+print(my_file.create_time)
+
+
+class File:
+    def __init__(self, name, create_time="today"):
+        self.name = name
+        self.create_time = create_time
+
+    def change_name(self, name):
+        self.name = name
+
+
+my_file = File("my_file")
+# 调用实例中，类的功能
+my_file.change_name("new_name")
+print(my_file.name)
+
+
+class File:
+    def __init__(self, name, create_time="today"):
+        self.name = name
+        self.create_time = create_time
+
+    def get_info(self):
+        return self.name + " is created at " + self.create_time
+
+
+my_file = File("my_file")
+print(my_file.get_info())
+
+
+class File:
+    def __init__(self, name, create_time="today"):
+        self.name = name
+        self.create_time = create_time
+
+    def get_info(self):
+        return self.name + " is created at " + self.create_time
+
+
+class Video(File):  # 继承了 File 的属性和功能
+    def __init__(self, name, window_size=(1080, 720)):
+        # 将共用属性的设置导入 File 父类
+        super().__init__(name=name, create_time="today")
+        self.window_size = window_size
+
+
+class Text(File):  # 继承了 File 的属性和功能
+    def __init__(self, name, language="zh-cn"):
+        # 将共用属性的设置导入 File 父类
+        super().__init__(name=name, create_time="today")
+        self.language = language
+
+    # 也可以在子类里复用父类功能
+    def get_more_info(self):
+        return self.get_info() + ", using language of " + self.language
+
+
+v = Video("my_video")
+t = Text("my_text")
+print(v.get_info())  # 调用父类的功能
+print(t.create_time)  # 调用父类的属性
+print(t.language)  # 调用自己的属性
+print(t.get_more_info())  # 调用自己加工父类的功能
+
+
+class File:
+    def __init__(self):
+        self.name = "f1"
+        self.__deleted = False  # 我不让别人用这个变量
+        self._type = "txt"  # 我不想别人使用这个变量
+
+    def delete(self):
+        self.__force_delete()
+
+    def __force_delete(self):  # 我不让别人使用这个功能
+        self.__deleted = True
+        return True
+
+    def _soft_delete(self):  # 我不想让别人使用这个功能
+        self.__force_delete()  # 我自己可以在内部随便调用
+        return True
+
+
+f = File()
+print(f._type)  # 可以拿到值，但是这个类的作者不想让你直接这样拿到
+print(f._soft_delete())  # 可以调用，但是这个类的作者不想让你直接调用
+
+# 接下来的两个实验都会报错
+f.__deleted
+f.__force_delete()
+print("<=======", "Class 类", "=======")
