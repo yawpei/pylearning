bb245807c958ce0b4ababdebbf1e57c91085b182
yaw <peiyw@yonyou.com>
2021-08-23 22:11:13 +0800

    基础学习：读写文件

--- test/files/a.py
+++ test/files/a.py
@@ -16,3 +16,40 @@ with open("new_file2.txt", "r") as f:
         print(line)
         if not line:
             break
+
+with open("chinese.txt", "wb") as f:
+    f.write("这是中文的，this is Chinese".encode("gbk"))
+
+with open("chinese.txt", "rb") as f:
+    print(f.read())
+
+# 下面的代码会报错
+with open("chinese.txt", "r") as f:
+    print(f.read())
+
+with open("chinese.txt", "r", encoding="gbk") as f:
+    print(f.read())
+
+# mode	意思
+# w	（创建）写文本
+# r	读文本，文件不存在会报错
+# a	在文本最后添加
+# wb	写二进制 binary
+# rb	读二进制 binary
+# ab	添加二进制
+# w+	又可以读又可以（创建）写
+# r+	又可以读又可以写, 文件不存在会报错
+# a+	可读写，在文本最后添加
+# x	创建
+
+with open("new_file.txt", "r") as f:
+    print(f.read())
+with open("new_file.txt", "r+") as f:
+    f.write("text has been replaced")
+    f.seek(0)  # 将开始读的位置从写入的最后位置调到开头
+    print(f.read())
+with open("new_file.txt", "a+") as f:
+    print(f.read())
+    f.write("\nadd new line")
+    f.seek(0)  # 将开始读的位置从写入的最后位置调到开头
+    print(f.read())
